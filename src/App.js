655de01b0b2f742205d071c5bf7e9b0e9c4b7692
Jcee Rage <burner.burn.burning@gmail.com>
2022-03-13 03:25:39 -0700

    configure router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
