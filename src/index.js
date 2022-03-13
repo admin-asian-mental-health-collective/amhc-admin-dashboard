4364bd1c7a2b28a9a64655a3d24b8cb9077fa905
Jcee Rage <burner.burn.burning@gmail.com>
2022-03-13 03:45:09 -0700

    add layout components on index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Layout>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Layout>,
   document.getElementById('root')
 );
 
