427a5cb520a6ebb3c4f4260017ec8c13562950d2
Jcee Rage <burner.burn.burning@gmail.com>
2022-03-13 03:28:01 -0700

    add wildcard path on router

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<div>Does Not Exist</div>} />
       </Routes>
     </div>
   );
