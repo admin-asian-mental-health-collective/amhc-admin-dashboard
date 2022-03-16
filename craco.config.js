a7832a155b5c3482e346bb8f12fce9dc1c9f4109
jcee-io <jcruz@asianmhc.org>
2022-03-16 02:28:08 -0700

    change spacing to 4 spaces

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,14 @@
 const CracoAlias = require('craco-alias')
 
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoAlias,
-      options: {
-        source: 'tsconfig',
-        baseUrl: './',
-        tsConfigPath: './tsconfig.path.json',
-      },
-    },
-  ],
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: 'tsconfig',
+                baseUrl: './',
+                tsConfigPath: './tsconfig.path.json',
+            },
+        },
+    ],
 }
