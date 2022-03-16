01162fe1e3f0fb9d118dc056f66a5ff86f85ae18
jcee-io <jcruz@asianmhc.org>
2022-03-16 02:11:09 -0700

    change file imports on relevant files

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from '@/reportWebVitals';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
+import Layout from '@components/Layout';
+import Home from '@pages/Home';
+import Dashboard from '@pages/Dashboard';
+import Login from '@pages/Login';
+import NotFound from '@pages/NotFound';
 
 ReactDOM.render(
     <Layout>
