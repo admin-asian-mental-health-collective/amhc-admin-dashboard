2c3c5cb643bcc74c5116fd03aaa091f0f6741efc
Jade Allen Cook <j_allencook@apple.com>
2022-09-18 03:25:16 -0700

    Completed navigation and added button atom

---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+import concatClassName from '../../../helpers/classNameConcat';
+import Link from '../Link';
+
+const Button = ({ className, to, children, ...rest }) =>
+	to ? (
+		<Link className={concatClassName('button', className)} {...rest}>
+			{children}
+		</Link>
+	) : (
+		<button className={concatClassName('button', className)} {...rest}>
+			{children}
+		</button>
+	);
+
+export default Button;
--- src/components/organisms/Navigation/index.js
+++ src/components/organisms/Navigation/index.js
@@ -5,6 +5,7 @@ import Grid from '../../atoms/Grid';
 import Box from '../../atoms/Box';
 import Image from '../../atoms/Image';
 import Link from '../../atoms/Link';
+import Button from '../../atoms/Button';
 
 import BannerLogo from '../../../assets/banner-logo.png';
 
@@ -17,8 +18,16 @@ export default function Navigation() {
 						<Image className="logo" src={BannerLogo} />
 					</Link>
 				</Box>
-				<Box></Box>
-				<Box>Dropdown</Box>
+				<Box>
+					<Link to="/directory">directory</Link>
+					<Link to="/resources">resources</Link>
+					<Link to="/blog">blog</Link>
+					<Link to="/about">about</Link>
+					<Link to="/therapists">contact</Link>
+				</Box>
+				<Box>
+					<Button to="/donate">donate</Button>
+				</Box>
 			</Grid>
 		</nav>
 	);
